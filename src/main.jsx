5cbac56d6b0a30504df58cdb02e7d53675786104
MJProgramming <joey.meierkord@gmail.com>
2023-09-20 19:04:05 +0200

    Add Roboto font for MUI

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./state/store";
 import Router from "./Router";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
